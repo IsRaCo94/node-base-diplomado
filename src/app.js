b50d78e475388d61256b1a7491dd298d58ab757e
Israel Bilbao La Vieja Bracamonte <bilisrael94@gmail.com>
2026-03-10 23:27:31 -0400

    Actualizando app.js con la ruta api-docs

--- src/app.js
+++ src/app.js
@@ -2,22 +2,27 @@ import express from 'express';
 import morgan from 'morgan';
 import usersRoutes from './routes/users.route.js';
 import authRoutes from './routes/auth.route.js';
-import tasksRoutes from './routes/tasks.route.js'; 
+import tasksRoutes from './routes/tasks.route.js';
+
+// 1. Nuevas importaciones para Swagger
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
 
 const app = express();
 
+// 2. Cargar el archivo yaml (Asegúrate de que swagger.yaml esté en la carpeta principal de tu proyecto)
+const swaggerDocument = yaml.load('./swagger.yaml');
+
+// Middlewares
 app.use(morgan('combined'));
 app.use(express.json());
 
+// Rutas de tu API
 app.use('/api/users', usersRoutes);
 app.use('/api/login', authRoutes);
-app.use('/api/tasks', tasksRoutes); 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 'online',
-        message: 'Israel Bilbao - Diplomado Full Stack - Proyecto Final',
-        message2: 'API funcionando correctamente',
-        docs: '/api/users, /api/login, /api/tasks'
-    });
-});
+app.use('/api/tasks', tasksRoutes);
+
+// 3. NUEVA RUTA: Aquí es donde el profesor verá la documentación
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 export default app;
